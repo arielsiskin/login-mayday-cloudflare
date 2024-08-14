6298e42b05378c804c01f65415b5c83f254280bd
arielsiskin <110627168+arielsiskin@users.noreply.github.com>
2024-08-14 11:52:03 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ module.exports = {
       },
       {
         source: '/api/proxy/ecd02/:path*',
-        destination: 'https://ecd02.neotelecd.com/:path*',
+        destination: 'http://ecd02.neotelecd.com/:path*',
       },
     ]
   },
